2fb108d03144306fc3ea266f5289947b3805dc1b
drafire <hyljljlj@163.com>
2026-06-19 18:37:40 +0800

    first commit

---
+++ rag-pgvector/src/main/java/com/drafire/RagPgVectorApplication.java
@@ -0,0 +1,11 @@
+package com.drafire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagPgVectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RagPgVectorApplication.class, args);
+    }
+}
