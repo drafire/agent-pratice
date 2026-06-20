acbf3e03b098eef4789f1682fb8d26055b092cef
drafire <hyljljlj@163.com>
2026-06-20 19:40:05 +0800

    1.增加了rerankmodel的说明

--- rag-pgvector/src/main/java/com/drafire/RerankConfig.java
+++ rag-pgvector/src/main/java/com/drafire/RerankConfig.java
@@ -19,6 +19,7 @@ public class RerankConfig {
         DashScopeApi dashScopeApi =DashScopeApi.builder()
                 .apiKey(apiKey)
                 .build();
+        //之所以选择qwen3-rerank是因为qwen3-rerank是qwen3的rerank模型。如果不配置这个，默认是gte-rerank模型，且不可以通过application.yml配置模型
         DashScopeRerankOptions options = DashScopeRerankOptions.builder()
                 .withModel("qwen3-rerank")
                 .build();
