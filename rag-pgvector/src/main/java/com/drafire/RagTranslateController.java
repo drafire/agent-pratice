461561bbb7f71a5f73835a7334d25c6510de8ec7
drafire <hyljljlj@163.com>
2026-06-20 23:19:36 +0800

    1.增加了rag的翻译功能

--- rag-pgvector/src/main/java/com/drafire/RagTranslateController.java
+++ rag-pgvector/src/main/java/com/drafire/RagTranslateController.java
@@ -2,7 +2,7 @@ package com.drafire;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
-import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer;
+import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,23 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rag/rewrite")
-public class RagRewriteController {
+@RequestMapping("/rag/translate")
+public class RagTranslateController {
     private final ChatClient chatClient;
     private final RetrievalAugmentationAdvisor retrievalAugmentationAdvisor;
 
-    public RagRewriteController(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
+    public RagTranslateController(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
         this.chatClient = chatClientBuilder.build();
         VectorStoreDocumentRetriever storeDocumentRetriever = VectorStoreDocumentRetriever.builder().vectorStore(vectorStore).similarityThreshold(0.5).build();
-        RewriteQueryTransformer queryTransformer = RewriteQueryTransformer.builder()
-                .chatClientBuilder(chatClientBuilder.build().mutate())
-                .targetSearchSystem("vector store")
+        TranslationQueryTransformer translateQueryTransformer = TranslationQueryTransformer.builder().chatClientBuilder(chatClientBuilder.build().mutate())
+                .targetLanguage("english")
                 .build();
-
-
         this.retrievalAugmentationAdvisor = RetrievalAugmentationAdvisor.builder()
-                .queryTransformers(queryTransformer)
                 .documentRetriever(storeDocumentRetriever)
+                .queryTransformers(translateQueryTransformer)
                 .build();
     }
 
